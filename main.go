bb3390d02c32fd3997bb7916e009a2df5c6dfae4
Ti <tianyizhang1987@gmail.com>
2018-12-10 22:25:18 -0800

    Adds 404 handler

--- main.go
+++ main.go
@@ -7,23 +7,11 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// func handlerFunc(w http.ResponseWriter, r *http.Request) {
-// 	w.Header().Set("Content-Type", "text/html")
-
-// 	if r.URL.Path == "/" {
-// 		fmt.Fprint(w, "<h1>Placeholder</h1>")
-// 	} else if r.URL.Path == "/contact" {
-// 		fmt.Fprint(w, "Reach out at "+"<a href= \"mailto: tianyizhang1987@gmail.com\">"+"tianyizhang1987@gmail.com</a>")
-// 	} else {
-// 		w.WriteHeader(http.StatusNotFound)
-// 		fmt.Fprint(w, "<h1>We could not find the page "+"were looking for:(</h1>"+"<p> Please email us if you keep being sent to an invalid page </p>")
-// 	}
-// }
-
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.NotFoundHandler = http.HandlerFunc(notFound)
 	http.ListenAndServe(":3000", r)
 }
 
@@ -35,4 +23,14 @@ func home(w http.ResponseWriter, r *http.Request) {
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "Reach out at "+"<a href= \"mailto: tianyizhang1987@gmail.com\">"+"tianyizhang1987@gmail.com</a>")
+}
+
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>FAQ</h1>"+"<p>Placeholder placeholder placeholder</p>")
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Page not found.</h1>")
 }
