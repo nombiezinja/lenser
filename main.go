413d56cd00295035af29fd462b55ddbff7c100d0
Ti <tianyizhang1987@gmail.com>
2018-12-10 22:32:29 -0800

    Trying out a different router package

--- main.go
+++ main.go
@@ -4,28 +4,29 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/gorilla/mux"
+	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", home)
-	r.HandleFunc("/contact", contact)
-	r.NotFoundHandler = http.HandlerFunc(notFound)
-	http.ListenAndServe(":3000", r)
+	router := httprouter.New()
+	router.GET("/", home)
+	router.GET("/contact", contact)
+	router.GET("/faq", faq)
+	router.NotFound = http.HandlerFunc(notFound)
+	http.ListenAndServe(":3000", router)
 }
 
-func home(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Placeholder</h1>")
 }
 
-func contact(w http.ResponseWriter, r *http.Request) {
+func contact(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "Reach out at "+"<a href= \"mailto: tianyizhang1987@gmail.com\">"+"tianyizhang1987@gmail.com</a>")
 }
 
-func faq(w http.ResponseWriter, r *http.Request) {
+func faq(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>FAQ</h1>"+"<p>Placeholder placeholder placeholder</p>")
 }
